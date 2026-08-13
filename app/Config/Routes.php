94a41c91770c60335a693ade8585611fc90ba39a
dwibagus777-spec <dwibagus777@gmail.com>
2026-08-13 22:13:29 +0700

    chore(debug): map master-assets/debug-foto to MigrateController for unauthenticated diagnostic fetching

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,8 +20,7 @@ $routes->get('queue/run', 'QueueWorker::run');
 
 // Production Database DDL Auto-Migrate & Catalog Seeder Route (Publicly callable for deployment sync)
 $routes->get('master-assets/auto-migrate', 'MigrateController::autoMigrate');
-$routes->get('master-assets/debug-foto/(:num)', 'AssetImportController::debugFoto/$1');
-$routes->get('debug-foto/(:num)', 'AssetImportController::debugFoto/$1');
+$routes->get('master-assets/debug-foto/(:num)', 'MigrateController::debugFoto/$1');
 
 // --- Rute Terproteksi Login (Protected Routes) ---
 $routes->group('', ['filter' => 'auth'], function ($routes) {

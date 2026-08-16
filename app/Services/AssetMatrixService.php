7ce755d19d07a4c2e412a31c6fcb220abd403e65
dwibagus777-spec <dwibagus777@gmail.com>
2026-08-16 09:14:32 +0700

    refactor(assets): refine AssetMatrixService to separate equipment sub-types (asset_types) from physical mounting structures (allowed_support_constructions)

--- app/Services/AssetMatrixService.php
+++ app/Services/AssetMatrixService.php
@@ -11,55 +11,64 @@ class AssetMatrixService
     {
         return [
             'JTM' => [
-                'name'                 => 'Jaringan Tegangan Menengah 20kV',
-                'allowed_constructions'=> ['TM1', 'TM2', 'TM3', 'TM4', 'TM5', 'TM6', 'TM7', 'TM8', 'TM9', 'TM10', 'TM11', 'TM12', 'TMMVTIC', 'SKTM', 'TMTP', 'TM16', 'TM16A', 'GTT', 'GTT1', 'GTT2', 'PMS', 'PMT'],
-                'default_construction' => 'TM1',
+                'name'                         => 'Jaringan Tegangan Menengah 20kV',
+                'asset_types'                  => ['TM1', 'TM2', 'TM3', 'TM4', 'TM5', 'TM6', 'TM7', 'TM8', 'TM9', 'TM10', 'TM11', 'TM12', 'TMMVTIC', 'SKTM', 'TMTP', 'TM16', 'TM16A', 'GTT', 'GTT1', 'GTT2', 'PMS', 'PMT'],
+                'allowed_support_constructions'=> ['TM1', 'TM2', 'TM3', 'TM4', 'TM5', 'TM6', 'TM7', 'TM8', 'TM9', 'TM10', 'TM11', 'TM12', 'TMTP', 'TM16', 'TM16A'],
+                'default_construction'         => 'TM1',
             ],
             'GARDU' => [
-                'name'                 => 'Gardu Distribusi 20kV',
-                'allowed_constructions'=> ['GD_PORTAL', 'GD_CANTOL', 'GD_BETON', 'GD_KIOS', 'GTT', 'GTT1', 'GTT2'],
-                'default_construction' => 'GD_PORTAL',
+                'name'                         => 'Gardu Distribusi 20kV',
+                'asset_types'                  => ['GD_PORTAL', 'GD_CANTOL', 'GD_BETON', 'GD_KIOS', 'GTT', 'GTT1', 'GTT2'],
+                'allowed_support_constructions'=> ['GD_PORTAL', 'GD_CANTOL', 'GD_BETON', 'GD_KIOS'],
+                'default_construction'         => 'GD_PORTAL',
             ],
             'TRAFO' => [
-                'name'                 => 'Trafo Distribusi 20kV',
-                'allowed_constructions'=> ['TRAFO_DISTRIBUSI', 'TM8', 'TM11', 'GD_PORTAL', 'GD_CANTOL'],
-                'default_construction' => 'TRAFO_DISTRIBUSI',
+                'name'                         => 'Trafo Distribusi 20kV',
+                'asset_types'                  => ['TRAFO_DISTRIBUSI', 'TRAFO_1PHASE', 'TRAFO_3PHASE'],
+                'allowed_support_constructions'=> ['TM8', 'TM11', 'GD_PORTAL', 'GD_CANTOL', 'GTT', 'GTT1', 'GTT2'],
+                'default_construction'         => 'TRAFO_DISTRIBUSI',
             ],
             'KUBIKEL' => [
-                'name'                 => 'Kubikel 20kV (Switchgear)',
-                'allowed_constructions'=> ['KUBIKEL_INCOMING', 'KUBIKEL_OUTGOING', 'KUBIKEL_METERING', 'KUBIKEL_PROTECTION', 'KUBIKEL'],
-                'default_construction' => 'KUBIKEL',
+                'name'                         => 'Kubikel 20kV (Switchgear)',
+                'asset_types'                  => ['KUBIKEL_INCOMING', 'KUBIKEL_OUTGOING', 'KUBIKEL_METERING', 'KUBIKEL_PROTECTION', 'KUBIKEL'],
+                'allowed_support_constructions'=> ['GD_BETON', 'GD_KIOS'],
+                'default_construction'         => 'KUBIKEL',
             ],
             'LBS' => [
-                'name'                 => 'Load Break Switch (LBS)',
-                'allowed_constructions'=> ['TM10', 'LBS_MANUAL', 'LBS'],
-                'default_construction' => 'LBS',
+                'name'                         => 'Load Break Switch (LBS)',
+                'asset_types'                  => ['LBS_MANUAL', 'LBS'],
+                'allowed_support_constructions'=> ['TM10', 'TM1', 'TM2'],
+                'default_construction'         => 'LBS',
             ],
             'LBSM' => [
-                'name'                 => 'LBS Motorized (SCADA)',
-                'allowed_constructions'=> ['LBS_MOTOR', 'LBSM'],
-                'default_construction' => 'LBSM',
+                'name'                         => 'LBS Motorized (SCADA)',
+                'asset_types'                  => ['LBS_MOTOR', 'LBSM'],
+                'allowed_support_constructions'=> ['TM10', 'TM16A'],
+                'default_construction'         => 'LBSM',
             ],
             'RECLOSER' => [
-                'name'                 => 'Automatic Circuit Recloser (ACR)',
-                'allowed_constructions'=> ['TM12', 'RECLOSER'],
-                'default_construction' => 'RECLOSER',
+                'name'                         => 'Automatic Circuit Recloser (ACR)',
+                'asset_types'                  => ['RECLOSER'],
+                'allowed_support_constructions'=> ['TM12'],
+                'default_construction'         => 'RECLOSER',
             ],
             'SECTIONALIZER' => [
-                'name'                 => 'Automatic Sectionalizer',
-                'allowed_constructions'=> ['SECTIONALIZER'],
-                'default_construction' => 'SECTIONALIZER',
+                'name'                         => 'Automatic Sectionalizer',
+                'asset_types'                  => ['SECTIONALIZER'],
+                'allowed_support_constructions'=> ['TM9', 'TM10'],
+                'default_construction'         => 'SECTIONALIZER',
             ],
             'JTR' => [
-                'name'                 => 'Jaringan Tegangan Rendah 380/220V',
-                'allowed_constructions'=> ['TR1', 'TR2', 'TR3', 'TR4', 'TR5', 'TR6', 'TR7', 'TR8', 'TR9', 'SKTR'],
-                'default_construction' => 'TR1',
+                'name'                         => 'Jaringan Tegangan Rendah 380/220V',
+                'asset_types'                  => ['TR1', 'TR2', 'TR3', 'TR4', 'TR5', 'TR6', 'TR7', 'TR8', 'TR9', 'SKTR'],
+                'allowed_support_constructions'=> ['TR1', 'TR2', 'TR3', 'TR4', 'TR5'],
+                'default_construction'         => 'TR1',
             ],
         ];
     }
 
     /**
-     * Check if a construction code is compatible with a primary asset family
+     * Check if a construction code is compatible with a primary asset family (as equipment type OR support construction)
      */
     public static function isCompatible(string $jenisAsset, string $constructionCode): bool
     {
@@ -71,6 +80,9 @@ class AssetMatrixService
             return true; // Allow custom asset categories gracefully
         }
 
-        return in_array($code, $matrix[$jenis]['allowed_constructions'], true);
+        $allowedTypes   = $matrix[$jenis]['asset_types'] ?? [];
+        $allowedSupport = $matrix[$jenis]['allowed_support_constructions'] ?? [];
+
+        return in_array($code, $allowedTypes, true) || in_array($code, $allowedSupport, true);
     }
 }
